dbde40862ea5506e3b5d02e99abd284a199af3a2
Shyam Vaya <vayashyam550@gmail.com>
2023-06-28 15:23:01 -0800

    day 10

--- day 8 js/myscript.js
+++
@@ -1,7 +0,0 @@
-function stringLength() 
-{
-     // Retrieving the values of form elements
-     
-     var name = document.getElementById("name").value;
-     document.getElementById('length').innerText = name.length;
-}
