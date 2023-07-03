c5173767bc0ce4cb17483b98dc54f07185b2c5c8
Shyam Vaya <vayashyam550@gmail.com>
2023-07-03 15:22:26 -0800

    Day 15

---
+++ day 15 assighment/myscipt.js
@@ -0,0 +1,13 @@
+
+var video = document.getElementById("fullscreen_bg_video");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
